dcb0c68fe4517d178270081ad41bfdb28bae4eca
Milankumar Patel <mils94patel@gmail.com>
2021-03-10 15:00:14 -0500

    flask application for greeting and calculation

---
+++ calc/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route("/add")
+def math_add():
+    """Add a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+
+    return str(result)
+
+@app.route("/sub")
+def math_sub():
+    """Subtract and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+
+    return str(result)
+
+@app.route("/mult")
+def math_mult():
+    """Multiply a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+
+    return str(result)
+
+@app.route("/div")
+def math_div():
+    """Divide a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+
+    return str(result)
+    
+## Make a single route/view function that can deal with 4 different kinds of URLs:
+
+#Route parameters for the actual operation
+operators = {
+        "add": add,
+        "sub": sub,
+        "mult": mult,
+        "div": div,
+        }
+
+@app.route("/math/<oper>")
+def math(oper):
+    """Do math on a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[oper](a, b)
+
+    return str(result)
